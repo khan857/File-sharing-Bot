f063b9ceeb1d2274a7740ada8e7da8d6a69a2c84
khan857 <mdaakib01786@gmail.com>
2024-09-27 05:13:24 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ : this is a private bot \n○ : Join telegram for meore <a href='tg://user?id={FORCE_SUB_CHANNEL}'>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
